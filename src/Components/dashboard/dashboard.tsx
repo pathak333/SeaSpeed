cab01baabd51f68a8c7c2d2328b09c23dbf1c74a
Abhaypathak31 <abhay.pathak@hdfcergo.com>
2023-07-28 20:47:27 +0530

    add new card on dash board and also show User Information

--- src/Components/dashboard/dashboard.tsx
+++ src/Components/dashboard/dashboard.tsx
@@ -4,10 +4,12 @@ import { useNavigate } from "react-router-dom";
 
 
 import DasboardCardLayout from "./dashboard_card_layout";
+import { useGlobalState } from "../../contexts/global.context";
+import { Description, DocumentScannerRounded } from "@mui/icons-material";
 const Dashboard = () => {
   const navigate = useNavigate();
-
-
+  const [globalState, dispatch] = useGlobalState();
+  let data = globalState.data != null ? globalState.data.data : null;
   useEffect(() => {
     window.history.pushState(null, "", window.location.pathname);
     console.log("dashboard component");
@@ -44,18 +46,22 @@ const Dashboard = () => {
         </div>
       </div>
     <DasboardCardLayout comeFrom="user" />
-    <div className="card flex flex-col p-3 ml-2 rounded-lg bg-white w-[260px] items-center">
-      <img src="" alt="seaSpeed Profile " className="p-3" />
+   <div id="bottomMenu" className="flex flex-row">
+   <div className="mb-5 mt-2 flex flex-col px-3 pt-3 ml-2 rounded-lg bg-white w-[260px] items-center">
+      <img src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="seaSpeed Profile " className="p-3 w-24 h-24 rounded-full" />
+     
+     <p id="name" className="h-5">{data !== null ? data.firstname : ""} {data != null ? data.lastname : ""}</p>
+      <p id="name" className="text-slate-400 font-semibold pb-2 text-sm">{data !== null ? data.vessel.label : ""}</p>
       <hr className=" w-full" />
-      <p id="name" className="p-3">Abhay pathak</p>
-      <hr className=" w-full" />
-      <p id="name" className="p-3">Vessel name</p>
-      <hr className=" w-full" />
-      <p id="name" className="p-3">SignIn Date</p>
-      <hr className=" w-full" />
-      <p id="name" className="p-3">SignOff Date</p>
-      <hr className=" w-full" />
+      <p id="name" className="p-2 text-sm">{data !== null ?data.joiningDate : ""}</p>
+      <hr className=" w-full " />
+      <p id="name" className="p-2 text-sm">{data !== null ?data.joiningPort : ""}</p>
+     
     </div>
+    <div id="agrement" className="mb-5 mt-2 flex flex-col px-3 pt-3 ml-2 rounded-lg bg-white w-[260px] items-center">
+    <Description width={"20px"}/>
+    </div>
+   </div>
     </div>
   );
 };
