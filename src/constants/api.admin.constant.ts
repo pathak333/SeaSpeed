bac451aff9a807b30766d75ab79e7677823e0884
Abhaypathak31 <abhay.pathak@hdfcergo.com>
2023-08-24 09:28:09 +0530

    add company profile vessel list service

--- src/constants/api.admin.constant.ts
+++ src/constants/api.admin.constant.ts
@@ -2,6 +2,8 @@
 export const GetAdminProfileApi = "/admin/getProfile";
 
 export const getAllVessel = "/admin/getAllVessel";
+export const getAllVesselByCompanyIdApi = "/admin/getAllVesselByCompanyId"
+
 export const addSubAdminApi = "/admin/addSubAdminUser";
 export const getSubAdminApi = "/admin/getAllSubAdmin"
 export const getAllCrewApi = "/admin/getAllCrew"
@@ -13,6 +15,7 @@ export const createNewUserApi = "/admin/addNewUser";
 export const addCompanyApi = "/admin/addCompany";
 export const addManagerApi = "/admin/addManager";
 export const getAllCompanyApi = "/admin/getAllCompany";
+
 export const getAllManagerByCompanyIdApi = "/admin/getAllManagerByCompanyId";
 export const addVesselApi = "/admin/addVessel";
 export const getAllVesselForAdminApi = "/admin/getAllVesselForAdmin"
